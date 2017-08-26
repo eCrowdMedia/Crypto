f02b251486b698cdbb79ceeaf4b21ef99d579c22
Willy Yeh <willy@ecrowdmedia.com>
2017-08-27 01:26:02 +0800

    Support Readium LCP decryption.

--- src/php/rsa_enc.php
+++ src/php/rsa_enc.php
@@ -1,5 +1,12 @@
 <?php
 	require 'vendor/autoload.php';
+	$loader = new \Composer\Autoload\ClassLoader();
+	$loader->addPsr4('phpseclib\\', 'vendor/phpseclib/phpseclib/phpseclib/');
+/*sd($loader);
+	$loader->register();
+*/
+	use phpseclib\Crypt\RSA;
+
 	$kpu =	"-----BEGIN PUBLIC KEY-----\n" .
 			"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIAuolYD2It7Va6XgGDocgiw19\n" .
 			"mt5OAHoK5aNaEyjfpdEKKaNgjs1SOZ0FLtwjUmdSUYcko363xh/yTpmFLn4jqmq+\n" .
